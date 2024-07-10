c7732c84e8f1cb0531af026f0e9dba16418ba579
Sunshine-Python <carena.wilson@outlook.com>
2024-07-10 19:19:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,8 @@ from strategies.atr import run_atr
 from strategies.standard_deviation import run_standard_deviation
 
 
-st.set_page_config(layout="wide", page_title="Little John")
+st.set_page_config(layout="wide")
+#st.set_page_config(layout="wide", page_title="Little John")
 logo_url = "little-john_logo.png"
 st.logo(logo_url)
 
