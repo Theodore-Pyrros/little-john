ad1c9075e9c228a6f0bc1d069c83fae084e21c09
Sunshine-Python <carena.wilson@outlook.com>
2024-07-11 01:24:44 +0200

    Update vwap.py

--- strategies/vwap.py
+++ strategies/vwap.py
@@ -6,6 +6,8 @@ from backtesting import Strategy
 from data_handler import fetch_data
 from utils import run_backtest, plot_strat_perf, display_metrics
 
+import matplotlib.font_manager as fm
+
 class VWAPStrategy(Strategy):
     vwap_periods = 20
     stop_loss_pct = 2.0
@@ -57,8 +59,14 @@ class VWAPStrategy(Strategy):
                 self.entry_price = self.data.Close[-1]
                 self.position_type = 'short'
 
+
+# Define font properties
+font_path = "Times New Roman.ttf"
+font_properties = fm.FontProperties(fname=font_path, size=14)
+title_font_properties = fm.FontProperties(fname=font_path, size=16, weight='bold')
+
 def vwap_viz(data, vwap_periods=20):
-    data = data[data['Volume'] > 0]    
+    data = data[data['Volume'] > 0].copy()
     data.reset_index(inplace=True)
     
     if 'Datetime' not in data.columns:
@@ -71,19 +79,42 @@ def vwap_viz(data, vwap_periods=20):
     data['Date'] = data['Datetime'].dt.date
     daily_indices = data.groupby('Date').first().index
     
-    fig, ax = plt.subplots(figsize=(14, 7))
-    ax.plot(data.index, data['Close'], label='Price', color='blue')
-    ax.plot(data.index, vwap, label=f'VWAP({vwap_periods})', color='red')
-    ax.set_title('VWAP Visualization')
-    ax.set_xlabel('Time')
-    ax.set_ylabel('Price')
+    fig, ax = plt.subplots(figsize=(14, 6), facecolor='none')
+
+    # Set transparent background
+    fig.patch.set_alpha(0)
+    ax.set_facecolor('none')
+
+    # Remove the outline of the axes
+    for spine in ax.spines.values():
+        spine.set_visible(False)
+
+    ax.plot(data.index, data['Close'], label='Price', color='green')  # Flashier pine green
+    ax.plot(data.index, vwap, label=f'VWAP({vwap_periods})', color='purple')
+    
+    ax.set_title('VWAP Visualization', fontproperties=title_font_properties, color='white')
+    ax.set_xlabel('Time', fontproperties=font_properties, color='white')
+    ax.set_ylabel('Price', fontproperties=font_properties, color='white')
+    
     ax.set_xticks([data[data['Date'] == date].index[0] for date in daily_indices])
-    ax.set_xticklabels([date.strftime('%Y-%m-%d') for date in daily_indices], rotation=30)
+    ax.set_xticklabels([date.strftime('%Y-%m-%d') for date in daily_indices], rotation=30, fontproperties=font_properties, color='white')
     
-    ax.legend()
-    ax.grid(True)
+    ax.tick_params(axis='x', colors='white', labelsize=12)
+    ax.tick_params(axis='y', colors='white', labelsize=12)
     
-    st.pyplot(fig)
+    for label in ax.get_xticklabels() + ax.get_yticklabels():
+        label.set_fontproperties(font_properties)
+
+    ax.legend(prop=font_properties, facecolor='white', framealpha=0.5)
+    ax.grid(True, axis='y', color='grey', linestyle='-', linewidth=0.5)
+    ax.grid(False, axis='x')
+    
+    plt.subplots_adjust(left=0.05, right=0.95, top=0.9, bottom=0.15)
+    plt.tight_layout()
+    st.pyplot(fig, clear_figure=True)
+
+
+
 
 def run_vwap(ticker, start_date, end_date, cash, commission, vwap_periods, stop_loss_pct, take_profit_pct, enable_shorting, enable_stop_loss, enable_take_profit):
     VWAPStrategy.vwap_periods = vwap_periods
