d22c04abb6c1020c6188b44d229192345bd01602
Sunshine-Python <carena.wilson@outlook.com>
2024-07-11 02:29:54 +0200

    Update momentum.py

--- strategies/momentum.py
+++ strategies/momentum.py
@@ -98,8 +98,12 @@ def momentum_viz(data, mom_period=14, mom_threshold=0):
     lines2, labels2 = ax2.get_legend_handles_labels()
     ax1.legend(lines1 + lines2, labels1 + labels2, loc='upper left', prop=font_properties)
 
-    ax1.set_xticks([data[data['Date'] == date].index[0] for date in daily_indices])
-    ax1.set_xticklabels([date.strftime('%Y-%m-%d') for date in daily_indices], rotation=30, fontproperties=font_properties, color='white')
+    # Limit the number of x-ticks to 7
+    xtick_locs = np.linspace(0, len(data) - 1, 7, dtype=int)
+    xtick_labels = [data.iloc[i]['Date'].strftime('%Y-%m-%d') for i in xtick_locs]
+
+    ax2.set_xticks(xtick_locs)
+    ax2.set_xticklabels(xtick_labels, rotation=30, fontproperties=font_properties, color='white')
     
     ax1.tick_params(axis='x', colors='white', labelsize=12)
     ax1.tick_params(axis='y', colors='white', labelsize=12)
