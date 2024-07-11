98d6ab15a6cdeaea191b9dc00b5993e56f0aa6e6
Sunshine-Python <carena.wilson@outlook.com>
2024-07-11 02:51:25 +0200

    Update standard_deviation.py

--- strategies/standard_deviation.py
+++ strategies/standard_deviation.py
@@ -119,7 +119,7 @@ def std_dev_viz(data, std_period=20, std_multiplier=2):
     xtick_labels = [data.iloc[i]['Date'].strftime('%Y-%m-%d') for i in xtick_locs]
 
     ax2.set_xticks(xtick_locs)
-    ax2.set_xticklabels(xtick_labels, rotation=30, fontproperties=font_properties, color='white')
+    ax2.set_xticklabels(xtick_labels, fontproperties=font_properties, color='white')
     
     ax1.tick_params(axis='x', colors='white', labelsize=12)
     ax1.tick_params(axis='y', colors='white', labelsize=12)
